3b70f39e7220b4f1666d8c997fb44f97850a7673
Pornpat Santibuppakul <dottypurkt@gmail.com>
2019-09-23 12:53:14 +0700

    Init repo (not done)

---
+++ src/main/java/th/ku/bookservice/BookRowMapper.java
@@ -0,0 +1,17 @@
+package th.ku.bookservice;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookRowMapper implements RowMapper<Book> {
+    public Book mapRow(ResultSet rs, int rowNum)
+            throws SQLException {
+        Book book = new Book(rs.getInt("id"),
+                rs.getString("name"),
+                rs.getDouble("price"));
+        return book;
+    }
+}
+
